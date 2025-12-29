5e37810155934cbb840d674b88438a3748edbba8
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-12-29 07:31:25 +0545

    Implement Best Fit memory allocation algorithm

---
+++ bestFit.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void bestFit(int blockSize[], int m, int processSize[], int n) {
+    int allocation[n];
+
+    for (int i = 0; i < n; i++)
+        allocation[i] = -1;
+
+    for (int i = 0; i < n; i++) {
+        int bestIdx = -1;
+
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                if (bestIdx == -1 || blockSize[j] < blockSize[bestIdx]) {
+                    bestIdx = j;
+                }
+            }
+        }
+
+        if (bestIdx != -1) {
+            allocation[i] = bestIdx;
+            blockSize[bestIdx] -= processSize[i];
+        }
+    }
+
+    printf("\nBest Fit Allocation:\n");
+    for (int i = 0; i < n; i++) {
+        if (allocation[i] != -1)
+            printf("Process %d -> Block %d\n", i + 1, allocation[i] + 1);
+        else
+            printf("Process %d -> Not Allocated\n", i + 1);
+    }
+}
+int main() {
+    
+    int blockSize2[] = {100, 500, 200, 300, 600};
+    
+
+    int processSize[] = {212, 417, 112, 426};
+
+    int m = 5;
+    int n = 4;
+
+    
+    bestFit(blockSize2, m, processSize, n);
+    
+
+    return 0;
+}
