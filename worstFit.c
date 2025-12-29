439e9aa771e3969e0ee4e5a325b744bc39db4ef3
Babin <155212596+Babin-exe@users.noreply.github.com>
2025-12-29 07:50:38 +0545

    Implement worst fit memory allocation algorithm

---
+++ worstFit.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+void worstFit(int blockSize[], int m, int processSize[], int n) {
+    int allocation[n];
+
+    for (int i = 0; i < n; i++)
+        allocation[i] = -1;
+
+    for (int i = 0; i < n; i++) {
+        int worstIdx = -1;
+
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                if (worstIdx == -1 || blockSize[j] > blockSize[worstIdx]) {
+                    worstIdx = j;
+                }
+            }
+        }
+
+        if (worstIdx != -1) {
+            allocation[i] = worstIdx;
+            blockSize[worstIdx] -= processSize[i];
+        }
+    }
+
+    printf("\nWorst Fit Allocation:\n");
+    for (int i = 0; i < n; i++) {
+        if (allocation[i] != -1)
+            printf("Process %d -> Block %d\n", i + 1, allocation[i] + 1);
+        else
+            printf("Process %d -> Not Allocated\n", i + 1);
+    }
+}
+
+int main() {
+    
+    int blockSize3[] = {100, 500, 200, 300, 600};
+    
+
+    int processSize[] = {212, 417, 112, 426};
+
+    int m = 5;
+    int n = 4;
+
+    
+    worstFit(blockSize3, m, processSize, n);
+    
+
+    return 0;
+}
